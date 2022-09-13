5f6c773a0b1274935f367c717a86ecae92248422
ronaldo-martinez <irmv_0311@hotmial.com>
2022-09-13 20:33:52 +0000

    nav

--- src/navbar/nav.jsx
+++ src/navbar/nav.jsx
@@ -1,29 +1,65 @@
-import {Routes, Route, Link } from 'react-router-dom';
+import {Routes, Route, NavLink } from 'react-router-dom';
 import Home from '../components/home';
 import Login from '../components/login';
 import Register from '../components/register';
+import '../App.css';
 
 function Nav() {
   return (
-    <>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div className="container-fluid">
+    <div className="bg-img1 vh-100">
+      <nav className="navbar navbar-light navbar-expand-md bg-faded justify-content-center pt-2">
+        <div className="w-100">
           <span className="navbar-brand"></span>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav justify-content-start">
               <li className="nav-item">
-                <Link className="nav-link active" aria-current="page" to="/">Home</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/login">Login</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/register">Register</Link>
+                <img className='px-5' src="/img/logo.svg" alt="" />
               </li>
             </ul>
+            <div className='w-100 justify-content-center'>
+              <div className='h-divider'></div>
+            </div>
+            <div className='w-100 justify-content-end'>
+            <ul className="navbar-nav w-100 justify-content-start bg-nav-c">
+              <li className="nav-item p-2">
+                <NavLink 
+                  className={({ isActive }) => (isActive ? 'p-2 nav-link text-white' : 'nav-link p-2')}
+                  aria-current="page" 
+                  to="/"
+                  >Home</NavLink>
+              </li>
+              <li className="nav-item p-2">
+                <NavLink 
+                  className={({ isActive }) => (isActive ? 'p-2 nav-link text-white' : 'nav-link p-2')} 
+                  aria-current="page" 
+                  to="/"
+                  >Destinos</NavLink>
+              </li>
+              <li className="nav-item p-2">
+                <NavLink className={({ isActive }) => (isActive ? 'p-2 nav-link text-white' : 'nav-link p-2')} 
+                aria-current="page" 
+                to="/"
+                >Tripulacion</NavLink>
+              </li>
+              <li className="nav-item  p-2">
+                <NavLink className="nav-link active" aria-current="page" to="/">Tecnología</NavLink>
+              </li>
+              <li className="nav-item p-2">
+                <NavLink className={({ isActive }) => (isActive ? 'p-2 nav-link text-white' : 'nav-link p-2')} 
+                to="/login"
+                >Login</NavLink>
+              </li>
+              <li className="nav-item p-2">
+                <NavLink className={({ isActive }) => (isActive ? 'p-2 nav-link text-white' : 'nav-link p-2')} 
+                to="/register" 
+                activeClassName="text-white"
+                >Register</NavLink>
+              </li>
+            </ul>
+            </div>
           </div>
         </div>
       </nav>
@@ -34,7 +70,7 @@ function Nav() {
           <Route path='/register' element={<Register></Register>} ></Route>
         </Routes>
       </div>
-    </>
+    </div>
   );
 }
 
