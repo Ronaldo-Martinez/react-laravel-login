c040d3607fc9eceb68c039650257b594acfaeddc
ronaldo-martinez <irmv_0311@hotmial.com>
2022-09-21 00:22:26 -0600

    solucion en error https

--- src/views/authUser.jsx
+++ src/views/authUser.jsx
@@ -33,7 +33,7 @@ export default function AuthUser(){
     }
 
     const http = axios.create({
-        baseURL: "http://code-rm.tk/api",
+        baseURL: "https://code-rm.tk/api",
         headers:{
             "content-type": "application/json",
             "Authorization":`Bearer ${token}`
